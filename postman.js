398583735dd9bd8dab0d1720d8d2f6e16238cc82
noah1365 <borlagdatanjohnlloyd1@gmail.com>
2025-02-21 14:51:11 +0800

    update

--- postman.js
+++ postman.js
@@ -1,3 +1,16 @@
+/* authRoute */
 // FOR REGISTRATION
-// http://localhost:7687/api/auth/register-account
+// http://localhost:7687/api/auth/register-account METHOD:POST
 
+// FOR GETTING ALL USERS
+// http://localhost:7687/api/auth/get-all-accounts  METHOD:GET
+
+// FOR LOGIN
+// http://localhost:7687/api/auth/login METHOD:POST
+
+// FOR LOGOUT
+// http://localhost:7687/api/auth/logout  METHOD:POST
+
+// FOR CHECK AUTH
+// http://localhost:7687/api/auth/check-auth  METHOD:GET
+ 
